9648e576749fb7d8eda69907575fbf1c1e97d903
farmonovsinno <farmonovsino2@gmail.com>
2024-02-28 17:12:33 +0500

    commit

---
+++ src/components/app/app.js
@@ -0,0 +1,21 @@
+import './app.css'
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+
+const App = () => {
+  return (
+    <div className='app font-monospace'>
+        <div className='content'>
+            <AppInfo />
+            <div className='search-panel'>
+                <SearchPanel />
+                <AppFilter />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
